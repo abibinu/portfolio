09986fc151420aee51e7b1aaf2516afc8ed715c7
abibinu <abikochu007@gmail.com>
2026-05-27 11:47:25 +0530

    add loading screen

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -40,8 +40,7 @@ const Loader = ({ finishLoading }) => {
       initial={{ opacity: 1 }}
       exit={{ 
         opacity: 0,
-        y: '-100%',
-        transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] }
+        transition: { duration: 0.6, ease: "easeInOut" }
       }}
       className="fixed inset-0 bg-black z-50 flex flex-col items-center justify-center font-sans overflow-hidden select-none"
     >
